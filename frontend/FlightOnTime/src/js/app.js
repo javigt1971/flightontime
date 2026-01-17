f6b0672a7d455752000bb57923fc529fc0245d3e
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-01-17 12:56:53 -0600

    Añadiendo id's y logica en JavaScript

--- frontend/FlightOnTime/src/js/app.js
+++ frontend/FlightOnTime/src/js/app.js
@@ -2,6 +2,9 @@ const modal = document.querySelector('#EquipoDialog');
 const llamarModal = document.querySelector('#equipo');
 const cerrarModal = document.querySelector('#btn-close');
 
+const resultado = document.querySelector('#resultado');
+const consultar = document.querySelector('#btn-action')
+
 
 llamarAddEventListeners();
 function llamarAddEventListeners() {
@@ -11,4 +14,10 @@ function llamarAddEventListeners() {
     cerrarModal.addEventListener('click', () => {
         modal.close();
     });
+}
+
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(contenedor.firstChild);
+    }
 }
