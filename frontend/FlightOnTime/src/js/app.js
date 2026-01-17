02aa1e27df5ffa58fbd6b95badb6aaae3e791409
Elias-J-Guardado <eliasjguardado2001@gmail.com>
2026-01-16 18:26:33 -0600

    Removiendo archivos generados, arreglando git ignore y modal funcionando

--- frontend/FlightOnTime/src/js/app.js
+++ frontend/FlightOnTime/src/js/app.js
@@ -1,1 +1,14 @@
-const 
+const modal = document.querySelector('#EquipoDialog');
+const llamarModal = document.querySelector('#equipo');
+const cerrarModal = document.querySelector('#btn-close');
+
+
+llamarAddEventListeners();
+function llamarAddEventListeners() {
+    llamarModal.addEventListener('click', () => {
+        modal.showModal()
+    });
+    cerrarModal.addEventListener('click', () => {
+        modal.close();
+    });
+}
